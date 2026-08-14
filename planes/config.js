083b678cb523d3e7f734d585e2862b23833caf4a
avunque <avunque@gmail.com>
2026-08-14 17:57:09 -0400

    Add files via upload

--- planes/config.js
+++ planes/config.js
@@ -1,7 +1,7 @@
-// Pega aquí las URLs CSV publicadas por Google Sheets para activar datos en vivo.
+// Google Sheets publicados como CSV. Esta es la fuente de datos en vivo.
 window.PLAN_CONFIG = {
-  cco460: { csvUrl: "" },
-  cco125: { csvUrl: "" },
-  cmu350: { csvUrl: "" },
-  inf105: { csvUrl: "" }
+  cco460: { csvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRFX-N6RMDlxZtK5tIlBuWvf-QVd_unbVUpCJrP9FU_rrHitJ2vKgVHmWEY9zyO6Q/pub?gid=835834855&single=true&output=csv" },
+  cco125: { csvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRFX-N6RMDlxZtK5tIlBuWvf-QVd_unbVUpCJrP9FU_rrHitJ2vKgVHmWEY9zyO6Q/pub?gid=830076875&single=true&output=csv" },
+  cmu350: { csvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRFX-N6RMDlxZtK5tIlBuWvf-QVd_unbVUpCJrP9FU_rrHitJ2vKgVHmWEY9zyO6Q/pub?gid=899461337&single=true&output=csv" },
+  inf105: { csvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRFX-N6RMDlxZtK5tIlBuWvf-QVd_unbVUpCJrP9FU_rrHitJ2vKgVHmWEY9zyO6Q/pub?gid=429772911&single=true&output=csv" }
 };
