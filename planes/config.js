2ccf0dafc9aefb22e9708e6882888558cdf4ee22
avunque <avunque@gmail.com>
2026-08-14 14:55:27 -0400

    Delete planes directory

--- planes/config.js
+++
@@ -1,7 +0,0 @@
-// Pega aquí las URLs CSV publicadas por Google Sheets para activar datos en vivo.
-window.PLAN_CONFIG = {
-  cco460: { csvUrl: "" },
-  cco125: { csvUrl: "" },
-  cmu350: { csvUrl: "" },
-  inf105: { csvUrl: "" }
-};
